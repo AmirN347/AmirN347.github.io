793a371767c19acebe91fdb30079b7dbbed8756b
AmirN347 <amirn2203@gmail.com>
2023-05-22 22:34:26 -0400

    Fixed React

--- Independent/independent.js
+++ Independent/independent.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./index1.css";
 
 export default function App() {
---
+++ Independent/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./independent";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+//root is how react mounts on the website, root is where we are telling react to add app
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+//follow this pattern when starting a new project
