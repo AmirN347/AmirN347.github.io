098d7d985cf2882f69036979386b386c2e1e68cc
AmirN347 <amirn2203@gmail.com>
2023-05-22 22:21:49 -0400

    Fixed React issues

--- Independent/independent.js
+++ Independent/independent.js
@@ -1,7 +1,11 @@
 import React from "react";
+
 import "./index1.css";
 
-
-export defualt function App() {
-
+export default function App() {
+  return (
+    <div className="container">
+      <h1>HEllo world</h1>
+    </div>
+  );
 }
